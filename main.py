f21621bfd3828c933700fb1024d3c3bfd03b56eb
mwbond <mwbond@gmail.com>
2012-04-09 17:51:17 -0400

    unpolished but working program

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import cStringIO
 import web
+from math import ceil
 
 import db
 
@@ -59,8 +60,58 @@ class intersection:
 		return render.intersection(kwargs)
 
 class update_intersection:
+	def calcYAR(self, length, speed, grade):
+		if (length==0) or (speed==0):
+			return '-', '-'
+		yellow = 1 + (0.733 * speed)/(10 + (0.32 * grade))
+		yellow = max(4.0, ceil(2 * yellow)/2)
+		red = length / speed
+		if ((ceil(2 * red)/2) - red) > 0.4:
+			red = ceil(2 * red)/2 - 0.5
+		else:
+			red = ceil(2 * red)/2
+		return str(yellow), str(red)
+	def calcFDW(self, length, red):
+		if length==0:
+			return '-'
+		if red=='-':
+			red = 0
+		pct = length/3.5
+		fdw = pct - max(float(red), 3.0)
+		fdw = pct - max(float(red), 3.0)
+		fdw = max(4.0, fdw)
+		fdw = ceil(2 * fdw)/2
+		return str(fdw)
 	def POST(self):
 		form = dict(web.input())
+		if 'p1_yar_len' in form:
+			for phase in ['1', '2', '3', '4', '5', '6', '7', '8']:
+				yar_length = form['p' + phase + '_yar_len']
+				if yar_length:
+					yar_length = int(float(yar_length))
+				else:
+					yar_length = 0
+				fdw_length = form['p' + phase + '_fdw_len']
+				if fdw_length:
+					fdw_length = int(float(form['p' + phase + '_fdw_len']))
+				else:
+					fdw_length = 0
+				speed = form['p' + phase + '_speed']
+				if speed:
+					speed = (float(speed) + 5)
+					speed = speed * 5280 / 3600
+				else:
+					speed = 0
+				grade = form['p' + phase + '_grade']
+				if grade:
+					grade = int(float(grade))
+				else:
+					grade = 0
+				yellow, red = self.calcYAR(yar_length, speed, grade)
+				fdw = self.calcFDW(fdw_length, red)
+				form['p' + phase + '_y'] = yellow
+				form['p' + phase + '_r'] = red
+				form['p' + phase + '_f'] = fdw
 		db.modify(**form)
 		raise web.seeother('/intersection' + '?IntID=' + form['int_id'])
 
